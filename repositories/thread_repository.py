ec8d8f7ea6c98de5455119254ab81475fdc6ac0b
AKelly404 <harley901@hotmail.com>
2023-03-29 21:06:22 +0100

    Added thread methods and tests

--- repositories/thread_repository.py
+++ repositories/thread_repository.py
@@ -1,4 +1,5 @@
 from typing import List
+from uuid import UUID
 
 from models.thread import Thread
 
@@ -9,3 +10,14 @@ class ThreadRepository:
 
     def add(self, thread: Thread):
         self.threads.append(thread)
+
+    def get_all_threads(self):
+        return self.threads
+
+    def get_thread(self, thread_id: UUID):
+        return next(thread for thread in self.threads if thread.id == thread_id)
+
+    #   Above next() code essentially operates as below
+    #     for threads in self.threads:
+    #         if threads.id == id:
+    #             return threads
--- test/test_thread_repository.py
+++ test/test_thread_repository.py
@@ -11,3 +11,18 @@ class TestThreadRepository:
         thread = Thread(users=[uuid4()], created_at=date, last_used_at=date)
         thread_repository.add(thread)
         assert thread_repository.threads[0].created_at == date
+
+    def test_thread_get_all(self):
+        thread_repository = ThreadRepository()
+        thread = Thread(users=[uuid4()], created_at=datetime.utcnow(), last_used_at=datetime.utcnow())
+        thread_repository.threads.insert(0, thread)
+        all_threads = thread_repository.get_all_threads()
+        assert len(thread_repository.get_all_threads()) == 1
+        assert all_threads[0].id == thread.id
+
+    def test_thread_get_one(self):
+        thread_repository = ThreadRepository()
+        thread = Thread(users=[uuid4()], created_at=datetime.utcnow(), last_used_at=datetime.utcnow())
+        thread_repository.threads.insert(0, thread)
+        one_thread = thread_repository.get_thread(thread.id)
+        assert one_thread.id == thread.id
